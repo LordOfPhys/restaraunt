b23de7ab79d0932a1c9438dc790dbaada484846e
LordOfPhys <acc951995@mail.ru>
2022-02-04 10:15:10 +0300

    Version 2.4. Change menu_photo from String to String[]

--- app/admin.py
+++ app/admin.py
@@ -4,4 +4,5 @@ from app.models import *
 admin.site.register(Restaraunt)
 admin.site.register(Table)
 admin.site.register(Photo)
+admin.site.register(MenuPhoto)
 # Register your models here.
--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ import json
 import random
 
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from app.models import *
 
@@ -10,12 +9,16 @@ def get_data(request):
     return json.loads(request.body.decode("utf-8"))
 
 def get_info_about_restaraunt(restaraunt):
+    menu_photos = MenuPhoto.objects.filter(restaraunt=restaraunt)
     photos = Photo.objects.filter(restaraunt=restaraunt)
+    arr_menu_photos = []
+    for photo in menu_photos:
+        arr_menu_photos.append(photo.get_image().url)
     arr_photos = []
     for photo in photos:
         arr_photos.append(photo.get_image().url)
     return json.dumps({'label': restaraunt.get_label(), 'description': restaraunt.get_description(),
-                       'label_photo': restaraunt.get_image_label().url, 'menu': restaraunt.get_menu().url,
+                       'label_photo': restaraunt.get_image_label().url, 'menu_photos': arr_menu_photos,
                        'photos': arr_photos})
 
 def get_unique_code_for_table():
